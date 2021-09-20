ae719f575a8e91d64f8ae05862852f51c1f134d9
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-09-20 19:34:39 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Laura Komp and is {""}
-        <a href="https://github.com/laurakomp/react-weather" target="_blank">
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Laura Komp and is{" "}
+          <a href="https://github.com/laurakomp/react-weather" target="_blank">
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
