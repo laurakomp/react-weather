772709012e1ac9f682fd3a490c59652bc396969f
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-10-03 19:47:25 +0300

    Minor fixes

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -23,7 +23,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div className="WeatherForecast-overview">
       <div className="WeatherForecast-day">{day()} </div>
-      <WeatherIcon code={props.data.weather[0].icon} height={20} />
+      <WeatherIcon code={props.data.weather[0].icon} />
 
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
